d77c427f62efc9b58e9c1e5e1abf6a29a55d04aa
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-06 18:35:25 +0200

    DoneAll

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,7 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
+	// Replace this comment with your code
 	int currentValue = Integer.parseInt(args[0]);
 	int years =Integer.parseInt(args[2]);
 	double rate = Double.parseDouble(args[1]);
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,7 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
+	// Replace this comment with your code
 	int number = Integer.parseInt(args[0]);
 	int hundreds =  number % 1000 / 100;
 	int tens = (number % 100) / 10;
