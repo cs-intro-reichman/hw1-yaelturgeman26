7520a1223c0e58dccd9784f4e16622dedf5c7047
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-06 17:48:48 +0200

    doneall

--- Demo0.java
+++ Demo0.java
@@ -1,6 +1,6 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-class Print5 {
+public class demo0 {
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
