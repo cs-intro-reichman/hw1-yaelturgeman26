ee925ec2c1693c0be045987be81e046d52f87695
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-07 14:57:53 +0200

    NumWords fix1

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 	// Replace this comment with your code
 	int number = Integer.parseInt(args[0]);
-	int hundreds =  number % 1000 / 100;
+	int hundreds = number / 100;
 	int tens = (number % 100) / 10;
 	int ones = (number % 10);
 	System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
