e9784aa3d220fdbd15f9ea0408bee3017a664981
Yael Turgeman <yael.turgeman@post.runi.ac.il>
2024-11-07 14:19:47 +0200

    Bill fix

--- Bill3.java
+++ Bill3.java
@@ -8,6 +8,6 @@ public class Bill3 {
 	    String name3 = args[2];
 		int BillAmount = Integer.parseInt(args[3]);
 		double total = Math.ceil((double) BillAmount / 3);
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + total + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + total + " Shekels each.");
 	}
 }
